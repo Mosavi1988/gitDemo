3c273554dfead531db5773273bef691979753225
Mosavi1988 <zh_mosavi@hotmail.com>
2020-08-01 11:51:36 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,6 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+        System.out.println("coding is complicated and at the same time interesting!");
     }
 }
