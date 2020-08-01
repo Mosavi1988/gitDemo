c174987c6f1cd7d4570ca3b04b0c9f186206b5fc
Mosavi1988 <67662860+Mosavi1988@users.noreply.github.com>
2020-08-01 16:35:15 -0400

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,7 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
         System.out.println("coding is complicated and at the same time interesting!");
+        // my shirt is dirty but yours is clean
+        // my ball is yellow and yours is green
     }
 }
